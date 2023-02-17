97344df6a761487587e6069d1ee44df5c9bd99ca
KF-Art <kevinfigueroart@protonmail.com>
2023-02-17 00:20:04 -0600

    Correct desktop-open syntax

--- desktop-open.sh
+++ desktop-open.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 PCMANFM_QT_CONF="$XDG_CONFIG_HOME/pcmanfm-qt/default/settings.conf"
+PCMANFM_QT_ETC_CONF="/etc/pcmanfm/default/settings.conf"
 XRANKMIRRORS_CONF="$XDG_CONFIG_HOME/xrankmirrors.conf"
+XRANKMIRRORS_ETC_CONF="/etc/xrankmirrors.conf"
 if [ -e $XRANKMIRRORS_CONF ]; then
     TERM_CMD="$(cat $XRANKMIRRORS_CONF | grep terminal | cut -d"=" -f2)"
+elif [ -e "$XRANKMIRRORS_ETC_CONF" ]; then
+	TERM_CMD="$(cat $XRANKMIRRORS_ETC_CONF | grep terminal | cut -d"=" -f2)"
 elif [ -e /usr/bin/exo-open ]; then
     TERM_CMD="exo-open --launch TerminalEmulator"
 elif [ -e $PCMANFM_QT_CONF ]; then
     TERM_CMD="$(cat $PCMANFM_QT_CONF | grep Terminal | cut -d"=" -f2)"
+elif [ -e "$PCMANFM_QT_ETC_CONF" ]; then
+	TERM_CMD="$(cat $PCMANFM_QT_ETC_CONF | grep Terminal | cut -d"=" -f2)"
 elif [ -e /usr/bin/lxterminal ]; then
     TERM_CMD="lxterminal"
 elif [ -e /usr/bin/mate-terminal ]; then
@@ -29,4 +35,4 @@ else
     exit 1
 fi
 
-pkexec env XAUTHORITY=${XAUTHORITY} DISPLAY=${DISPLAY} $TERM_CMD -e xrankmirrors -i
+pkexec env XAUTHORITY=${XAUTHORITY} QT_QPA_PLATFORMTHEME=${QT_QPA_PLATFORMTHEME} DISPLAY=${DISPLAY} $TERM_CMD -e "xrankmirrors -i"

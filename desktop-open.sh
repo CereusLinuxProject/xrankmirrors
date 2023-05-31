96f44e5cb22702bbc601d041f67203910324553f
KF-Art <kevinfigueroart@protonmail.com>
2023-05-31 19:36:22 -0400

    Update desktop-open.sh with many improvements: - Fix user config reading. - Add support for additional arguments in custom config. - Manage arguments and terminal command in different variables. - Now it uses a wrapper script for launching pkexec. - Terminal emulator now is run as user instead of root. - Now it changes the terminal title if supported. - Added qterminal and xfce4-terminal support.

--- desktop-open.sh
+++ desktop-open.sh
@@ -1,20 +1,31 @@
 #!/bin/sh
+XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
 PCMANFM_QT_CONF="$XDG_CONFIG_HOME/pcmanfm-qt/lxqt/settings.conf"
 PCMANFM_QT_ETC_CONF="/etc/pcmanfm-qt/lxqt/settings.conf"
 XRANKMIRRORS_CONF="$XDG_CONFIG_HOME/xrankmirrors.conf"
 XRANKMIRRORS_ETC_CONF="/etc/xrankmirrors.conf"
-if [ -e $XRANKMIRRORS_CONF ]; then
+TERM_TITLE="xrankmirrors"
+TERM_ARGS=""
+RUN_CMD="/usr/share/xrankmirrors/pkexec-launch"
+
+if [ -e "$XRANKMIRRORS_CONF" ]; then
     TERM_CMD="$(cat $XRANKMIRRORS_CONF | grep terminal | cut -d"=" -f2)"
+    TERM_ARGS="$(cat $XRANKMIRRORS_CONF | grep args | cut -d"=" -f2)"
 elif [ -e "$XRANKMIRRORS_ETC_CONF" ]; then
 	TERM_CMD="$(cat $XRANKMIRRORS_ETC_CONF | grep terminal | cut -d"=" -f2)"
+	TERM_ARGS="$(cat $XRANKMIRRORS_ETC_CONF | grep args | cut -d"=" -f2)"
 elif [ -e /usr/bin/exo-open ]; then
-    TERM_CMD="exo-open --launch TerminalEmulator"
+    TERM_CMD="exo-open"
 elif [ -e $PCMANFM_QT_CONF ]; then
     TERM_CMD="$(cat $PCMANFM_QT_CONF | grep Terminal | cut -d"=" -f2)"
 elif [ -e "$PCMANFM_QT_ETC_CONF" ]; then
 	TERM_CMD="$(cat $PCMANFM_QT_ETC_CONF | grep Terminal | cut -d"=" -f2)"
+elif [ -e /usr/bin/xfce4-terminal ]; then
+	TERM_CMD="xfce4-terminal"
 elif [ -e /usr/bin/lxterminal ]; then
     TERM_CMD="lxterminal"
+elif [ -e /usr/bin/qterminal ]; then
+	TERM_CMD="qterminal"
 elif [ -e /usr/bin/mate-terminal ]; then
     TERM_CMD="mate-terminal"
 elif [ -e /usr/bin/konsole ]; then
@@ -35,4 +46,30 @@ else
     exit 1
 fi
 
-pkexec env XAUTHORITY=${XAUTHORITY} QT_QPA_PLATFORMTHEME=${QT_QPA_PLATFORMTHEME} DISPLAY=${DISPLAY} $TERM_CMD -e "xrankmirrors -i"
+case $TERM_CMD in
+	exo-open)
+		TERM_ARGS="--launch TerminalEmulator" ;;
+	lxterminal)
+		TERM_ARGS="-T $TERM_TITLE -e" ;;
+	mate-terminal)
+		TERM_ARGS="-t $TERM_TITLE -e" ;;
+	konsole)
+		TERM_ARGS="-T $TERM_TITLE -e" ;;
+	gnome-terminal)
+		TERM_ARGS="-t $TERM_TITLE --" ;;
+	urxvt)
+		TERM_ARGS="-T $TERM_TITLE -e" ;;
+	xfce4-terminal)
+		TERM_ARGS="-T $TERM_TITLE -e" ;;
+	kitty)
+		TERM_ARGS="-T $TERM_TITLE" ;;
+	tilix)
+		TERM_ARGS="-t $TERM_TITLE -e" ;;
+	xterm)
+		TERM_ARGS="-T $TERM_TITLE -e" ;;
+	qterminal)
+		# Qterminal does not support to change the title through launch arguments.
+		TERM_ARGS="-e" ;;
+esac
+
+exec $TERM_CMD $TERM_ARGS ${RUN_CMD}
